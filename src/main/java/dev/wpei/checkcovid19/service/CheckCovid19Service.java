ecde48884df5463c9fa4bf41a7c3fe526f22cb78
WATANAPEI <info@wpei.dev>
2021-08-21 00:28:32 +0900

    add s3 upload feature

--- src/main/java/dev/wpei/checkcovid19/service/CheckCovid19Service.java
+++ src/main/java/dev/wpei/checkcovid19/service/CheckCovid19Service.java
@@ -10,15 +10,23 @@ import dev.wpei.checkcovid19.model.Item;
 import dev.wpei.checkcovid19.model.Response;
 import lombok.extern.slf4j.Slf4j;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.profiles.Profile;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.io.Writer;
+import java.io.*;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.UUID;
 
 
 @Slf4j
@@ -45,8 +53,12 @@ public class CheckCovid19Service {
         // get response
         Response response = fetchPatientsNumberPerDate(dateOfData);
         //save response data as csv file
-        saveCsvToLocal(response.getItemList(), todayString);
+        final String saveFilePath = "target/patients" + todayString + ".csv";
+        File saveFile = new File(saveFilePath);
+        saveCsvToLocal(response.getItemList(), saveFile);
+        saveCsvToS3(response.getItemList(), saveFile);
     }
+
     private String generateDateFrom(int minusDay) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
         OffsetDateTime dayBeforeYesterday = OffsetDateTime.now().minusDays(minusDay);
@@ -68,13 +80,50 @@ public class CheckCovid19Service {
         return response;
 
     }
-    private void saveCsvToLocal(List<Item> itemList, String todayString) {
-        final String saveFilePath = "target/patients" + todayString + ".csv";
+    private void saveCsvToS3(List<Item> itemList, File saveFile) {
+        String bucketName = "lambda-artifacts-fs2wafw43";
+        String objectKey = UUID.randomUUID().toString();
+        saveCsvToLocal(itemList, saveFile);
+        log.debug("Putting object "+ objectKey +" into bucket "+bucketName);
+        ProfileCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create("default");
+        try(S3Client s3Client = S3Client.builder()
+                .region(Region.US_EAST_2)
+                .credentialsProvider(credentialsProvider)
+                .build();) {
+
+            try {
+                String result = putS3Object(s3Client, bucketName, objectKey, saveFile);
+                log.debug("Tag info: " +result);
+
+
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    private String putS3Object(S3Client s3, String bucketName, String objectKey, File localFile) throws IOException {
+        try {
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(objectKey)
+                    .build();
+
+            PutObjectResponse putObjectResponse = s3.putObject(putObjectRequest, RequestBody.fromFile(localFile));
+            return putObjectResponse.eTag();
+        } catch(S3Exception e) {
+            throw new IOException(e);
+        }
+    }
+    private void saveCsvToLocal(List<Item> itemList, File saveFilePath) {
         log.debug("Translating response to csv...");
         try ( Writer writer = new FileWriter(saveFilePath)) {
             StatefulBeanToCsv beanToCsv = new StatefulBeanToCsvBuilder(writer).build();
             try {
                 beanToCsv.write(itemList);
+                writer.close();
                 log.debug("Translation to csv finished.");
             } catch (CsvDataTypeMismatchException | CsvRequiredFieldEmptyException e) {
                 throw new IllegalArgumentException(e);
